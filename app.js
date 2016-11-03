bd4222f45c7a9738093885d7cade5a3e7914a384
David Rips <davidrips@github.com>
2016-11-03 16:16:36 -0400

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var mysql = require('mysql');
 var cookieParser = require('cookie-parser');
