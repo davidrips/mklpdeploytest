dffacd71b9925e25cd1bc2329a661df6595637e7
David Rips <davidrips@github.com>
2016-11-21 14:46:00 -0500

    sweeps3

--- public/js/sweeps.js
+++ public/js/sweeps.js
@@ -1,5 +1,42 @@
 $(document).ready(function(){
 
+
+
+
+
+
+
+
+    $("#fbButton").on('click', function(){
+            console.log('clicked fb button');
+            loggingIn()
+
+    })
+
+  function loggingIn(){
+      if(document.getElementById('agree').checked){ 
+        FB.login(function(response) {
+            if (response.authResponse) {
+
+              console.log('Welcome!  Fetching your information.... ');
+              FB.api('/me', {fields: 'name,email'}, function(response) {
+                console.log(response);
+                console.log(response.email);
+                successTest(response.email);
+               });
+            }else{
+               console.log('User cancelled login or did not fully authorize.');
+            }
+          }, {scope: "public_profile,email"});
+        return false; 
+      } else{ 
+      alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy'); 
+      return false; 
+    }        
+  }
+
+
+
     $(function(){
     	$('.box').matchHeight();
     })
@@ -36,7 +73,7 @@ $(document).ready(function(){
 
     	$.post('/sweepstake', {email: email}, function(data, textStatus, xhr) {
     		console.log('submitted successfully!');
-    		console.log(data);
+    		// console.log(data);
     		$("#successModal").modal();
     	});
 
