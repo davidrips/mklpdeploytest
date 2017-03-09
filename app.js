fdb380015aa20194f9fede6c83e3a81826ae14b8
David Rips <davidrips@github.com>
2017-03-09 17:53:41 -0500

    new updates

--- app.js
+++ app.js
@@ -32,22 +32,22 @@ redirect(app);
 
 // app.use('/', routes);
 
-// var Sequelize = require('sequelize');
+var Sequelize = require('sequelize');
 
 // DEPLOY TEST LIVE//
-var pg = require('pg');
+// var pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+// pg.defaults.ssl = true;
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   if (err) throw err;
+//   console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+//   client
+//     .query('SELECT table_schema,table_name FROM information_schema.tables;')
+//     .on('row', function(row) {
+//       console.log(JSON.stringify(row));
+//     });
+// });
 
 // DEPLOY TEST LIVE//
 var sequelize = new Sequelize(process.env.DATABASE_URL, {
